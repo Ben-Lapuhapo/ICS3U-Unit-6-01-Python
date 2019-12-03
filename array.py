acaa192dbb3aeacf8af019668f3dc697f638082b
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-12-03 00:23:02 +0000

    Added arrays for random generator and average calculator

---
+++ array.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Created by : Ben Lapuhapo
+# Created on : December 02 2019
+# This program uses arrays
+
+import random
+
+
+def main():
+    # this function uses an array
+    random_number = []
+    number_of_integers = 10
+
+    # output and process
+    for loop_counter in range(0, 11):
+        total = sum(random_number)
+        random_numbers = (random.randint(1, 101))
+        random_number.append(random_numbers)
+
+    print("")
+    print("Here are the Numbers:")
+    for loop_counter in range(0, 10):
+        average = total / number_of_integers
+        print("{0} ".format(random_number[loop_counter]), end="")
+
+    print('\n')
+    print("Average is: {0}".format(average))
+
+
+if __name__ == "__main__":
+    main()
